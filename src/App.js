b3150d5763b5edd5d0a1cec48a3e1b4c4c16d827
Srush123 <srushtibhari2000@gmail.com>
2021-07-27 20:57:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Aboutus from './Aboutus';
+import Contactus from './Contactus';
+import {BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+
+
+<Router>
+    <div className="App">
+      <Navbar />
+      <Switch>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/Aboutus" component={Aboutus}></Route>
+      <Route path="/Contactus" component={Contactus}></Route>
+
+      </Switch>
+    </div>
+    </Router>
+
+ {/* );
+}*/}
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
